612a038b5758cdcd64fd8e4651262341c8542f87
sademakhubele <157542678+sademakhubele@users.noreply.github.com>
2024-04-12 15:06:45 +0200

    added basic search engine no API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-let searchBarElement = document.querySelector("search-bar");
-console.log("searchBarElement");
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+
+}
+
+let searchBarElement = document.querySelector("#search-bar");
+searchBarElement.addEventListener("submit",handleSearchSubmit);
