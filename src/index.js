2ceb103f0746e92e7d729db547799972cf8f12d9
sademakhubele <157542678+sademakhubele@users.noreply.github.com>
2024-04-15 13:02:06 +0200

    updated javascript

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+function searchCity(city) {
+    let appiKey = "b3246aecf7f467401t42442f8b6f3f8o";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key={apiKey}&units=metric`;
+
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
+    let searchInput = document.querySelector("#search-bar-input");
     
-    let cityElement = document.querySelector("#city");
+    let cityElement = document.querySelector("#app-city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 
 }
 
